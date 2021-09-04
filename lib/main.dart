8ee73198e7d5299e12ba8ab35bde0b6af3795efd
Bader Albader <b.albader94@gmail.com>
2021-09-04 15:30:02 +0300

    Buttons and Die Images
    - Turned die into buttons.
    - Changed image widget to image.assets to simplify code.
    - Put all in expanded to take up screen size and restrict to screen size.
    - Added then removed padding since TextButton comes with default 16.0 padding on right and left.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DicePage());
-}
-
-class DicePage extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Dicee',
       home: Scaffold(
         backgroundColor: Colors.red,
@@ -16,7 +10,35 @@ class DicePage extends StatelessWidget {
           backgroundColor: Colors.red,
           title: Text('Dicee'),
         ),
-        body: Container(),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Left button got pressed!');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Right button got pressed!');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
